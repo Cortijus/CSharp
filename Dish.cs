700110e2953597b8714dd87b43979829a20f1616
Dream Miller <139028280+Cortijus@users.noreply.github.com>
2023-11-03 21:03:07 -0500

    Add files via upload

---
+++ Dish.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MenuGenerator
+{
+
+
+    public class Dish
+    {
+        private string dishName;
+        private int dishPrice, dishType;
+
+
+
+
+        //const with param
+        public Dish(string dishName = " ", int dishType = 0, int dishPrice = 0)
+        {
+            SetName(dishName);
+            SetType(dishType);
+            SetPrice(dishPrice);
+        }
+
+
+
+        //GETTERS
+
+        /** Returns the name of the dish.
+            @return: dish name
+*/
+
+        public string GetName() { return dishName; }
+
+
+        /** Returns the type of dish ordered.
+            @return: returns either appetizer, entree, or dessert
+*/
+
+        public new int GetType() { return dishType; }
+
+        /** Returns the price of the dish.
+            @return: dish price
+*/
+
+        public int GetPrice() { return dishPrice; }
+
+
+        //SETTERS
+
+        /** Updates name of dish.
+            @param width: the updated dish name
+*/
+
+        public void SetName(string dishName) { this.dishName = dishName; }
+
+        /** Updates the type of dish.
+            @param width: the updated dish type
+*/
+        public void SetType(int dishType) { this.dishType = dishType; }
+
+        /** Updates the type of dish.
+            @param width: the updated dish type
+*/
+
+        public void SetPrice(int dishPrice) { this.dishPrice = dishPrice; }
+
+        /** Customizes the output format of the menu.
+            @return: a string representing the output format of the menu
+         */
+
+        public override string ToString() { return string.Format(dishName + " ($" + dishPrice + ")", GetName(), GetPrice()); }
+
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+
+
+namespace MenuGenerator
+{
+    class HellsKitchen
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+            //Opening and creating input and output file
+            FileStream inputFile = new FileStream("Dishes.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(inputFile);
+            FileStream outputFile = new FileStream("Menu.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outputFile);
+
+
+            //creating lists for dish types
+            List<Dish> appetizer = new List<Dish>();
+            List<Dish> entree = new List<Dish>();
+            List<Dish> dessert = new List<Dish>();
+
+
+            //while loop to read entire input file
+            while (!reader.EndOfStream)
+            {
+
+                string[] nextLine = reader.ReadLine().Split('\t', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+                int dishType1 = 0;
+                string dishName = nextLine[2];
+                int dishPrice = Convert.ToInt32(nextLine[1]);
+                string dishType = nextLine[0];
+                Dish dish = new Dish();
+
+                //conditional for dish types
+                if (dishType == "Appetizer")
+                {
+                    dishType1 = 0;
+                    dish.SetType(dishType1);
+                    dish.SetName(dishName);
+                    dish.SetPrice(dishPrice);
+                    appetizer.Add(dish);
+                }
+                else if (dishType == "Entree")
+                {
+                    dishType1 = 1;
+                    dish.SetType(dishType1);
+                    dish.SetName(dishName);
+                    dish.SetPrice(dishPrice);
+                    entree.Add(dish);
+                }
+                else if (dishType == "Dessert")
+                {
+                    dishType1 = 2;
+                    dish.SetType(dishType1);
+                    dish.SetName(dishName);
+                    dish.SetPrice(dishPrice);
+                    dessert.Add(dish);
+                }
+
+                
+            }
+
+            //formatting and displaying list contents for menu
+            writer.WriteLine("Menu\n");
+            writer.WriteLine("Appetizer\n");
+
+            for (int i = 0; i < appetizer.Count; i++)
+            {
+                writer.WriteLine(appetizer[i]);
+            }
+
+            
+            writer.WriteLine("\nEntree\n");
+
+            for (int i = 0; i < entree.Count; i++)
+            {
+                writer.WriteLine(entree[i]);
+            }
+
+            writer.WriteLine("\nDessert\n");
+
+            for (int i = 0; i < dessert.Count; i++)
+            {
+                writer.WriteLine(dessert[i]);
+            }
+
+
+
+
+
+            // Close the input and output file.
+            writer.Close();
+            outputFile.Close();
+            reader.Close();
+            inputFile.Close();
+
+
+
+
+
+
+        }
+    }
+
+}
